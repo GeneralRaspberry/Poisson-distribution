52ae1d19e2956e32bc41e4a195ecbf7b1a220b77
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-06 20:30:34 +0100

    Add files via upload

--- poisson experiment.R
+++ poisson experiment.R
@@ -19,3 +19,9 @@ plot(p3)
 hist(p3)
 
 ################################adding a poisson for out sum(!landcape marks)#################################
+
+p4<-rpois(n=sum(!landscape$marks),lambda = 5)
+
+plot(p4)
+
+hist(p4)
