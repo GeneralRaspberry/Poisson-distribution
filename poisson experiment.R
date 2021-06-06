db3f4aba871935d5da80b0bbab5023c9c11b9712
Thomas Taylor <t.h.taylor@outlook.com>
2021-06-06 20:27:47 +0100

    Add files via upload

---
+++ poisson experiment.R
@@ -0,0 +1,21 @@
+#######################poisson distribution experimentation#####################################################
+
+p1<-rpois(40,20)
+
+plot(p1)
+
+hist(p1)
+
+p2<-rpois(70,5)
+
+plot(p2)
+
+hist(p2)
+
+p3<-rpois(35,1)
+
+plot(p3)
+
+hist(p3)
+
+################################adding a poisson for out sum(!landcape marks)#################################
